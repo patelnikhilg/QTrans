0bc5cd9b9e4c4cb3ab9f937d8122c246c11bc815
patelnikhilg <43097771+patelnikhilg@users.noreply.github.com>
2018-09-11 17:13:09 +0530

    resolved error and add display name

--- Src/Web/QTrans.WebPortal/Models/Login/Login.cs
+++ Src/Web/QTrans.WebPortal/Models/Login/Login.cs
@@ -7,17 +7,19 @@ using System.Web;
 namespace QTrans.WebPortal.Models.Login
 {
     public class Login
-    {
-        [Requi]
+    {        
         [DisplayName("User Name")]
         public string UserName { get; set; }
+        [DisplayName("Password")]
         public string Password { get; set; }
 
     }
 
     public class Forgotpassword
     {
+        [DisplayName("Email Address")]
         public string EmailAddress { get; set; }
+        [DisplayName("Mobile No.")]
         public string MobileNo { get; set; }
     }
 }
