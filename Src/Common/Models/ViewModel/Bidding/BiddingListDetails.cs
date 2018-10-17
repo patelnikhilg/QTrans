1d844ddc0e1c14b1f24f36000216af4b1e7f8e14
patelnikhilg <43097771+patelnikhilg@users.noreply.github.com>
2018-10-17 22:07:09 +0530

    Made changes in GetBiddingListByDtPostingId API call
    Add Status, Rating Avg and total rating to user

--- Src/Common/Models/ViewModel/Bidding/BiddingListDetails.cs
+++ Src/Common/Models/ViewModel/Bidding/BiddingListDetails.cs
@@ -5,12 +5,16 @@ namespace QTrans.Models.ViewModel.Bidding
 {
     public class BiddingListDetails
     {
+        public long userid { get; set; }
         public string firstname { get; set; }
         public string lastname { get; set; }
         public long biddingid { get; set; }
         public decimal amount { get; set; }
         public string biddercomment { get; set; }
         public string rating { get; set; }
+        public int statu { get; set; }
+        public int RatingAvg { get; set; }
+        public int TotalRating { get; set; }
         public DateTime LastModify { get; set; }
 
         public ICollection<BiddingDetails> biddingDetails { get; set; }
